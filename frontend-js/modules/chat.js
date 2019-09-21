10af099be87ac8c0ecf0523d595f3d46ed49fd2a
Don Yu <devzons@gmail.com>
2019-09-20 19:02:52 -0700

    Chat: completed chat app

--- frontend-js/modules/chat.js
+++ frontend-js/modules/chat.js
@@ -24,6 +24,17 @@ export default class Chat {
   // Methods
   sendMessageToServer() {
     this.socket.emit('chatMessageFromBrowser', {message: this.chatField.value})
+    this.chatLog.insertAdjacentHTML('beforeend', `
+    <div class="chat-self">
+      <div class="chat-message">
+        <div class="chat-message-inner">
+          ${this.chatField.value}
+        </div>
+      </div>
+      <img class="chat-avatar avatar-tiny" src="${this.avatar}">
+    </div>
+    `)
+    this.chatLog.scrollTop = this.chatLog.scrollHeight
     this.chatField.value = ''
     this.chatField.focus()
   }
@@ -38,10 +49,15 @@ export default class Chat {
     }
     this.openedYet = true
     this.chatWrapper.classList.add("chat--visible")
+    this.chatField.focus()
   }
 
   openConnection() {
     this.socket = io()
+    this.socket.on('welcome', data => {
+      this.username = data.username
+      this.avatar = data.avatar
+    })
     this.socket.on('chatMessageFromServer', data => {
       this.displayMessageFromServer(data)
     })
@@ -56,6 +72,7 @@ export default class Chat {
       ${data.message}
     </div></div>
   </div>`)
+  this.chatLog.scrollTop = this.chatLog.scrollHeight
   }
 
   injectHTML() {
